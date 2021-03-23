1dc98bf224294ad243e790202a66214477ebf216
Matheus Lima <matheus.limastack@outlook.com>
2021-03-23 18:44:57 -0300

    Completed triangle project

--- triangle.rb
+++ triangle.rb
@@ -13,10 +13,43 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+refactoring_approach = <<-EOF
+ we can refactoring this triangle method
+ to use a different approach, here is my second suggestion
+
+ def triangle_type
+  {
+    1 => :equilateral,
+    2 => :isosceles,
+    3 => scalene
+  }
+ end
+
+ def triangle(a,b,c)
+  triangle_type[[a,b,c].uniq.size]
+ end
+EOF
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if equilateral? a,b,c 
+  return :isosceles if isosceles? a,b,c
+  :scalene if scalene? a,b,c
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+end
+
+def scalene?(a,b,c)
+  [a,b,c].uniq.length == 3
+end
+
+def isosceles?(a,b,c)
+  [a,b,c].uniq.length == 2
+end
+
+def equilateral?(a,b,c)
+  [a,b,c].uniq.length == 1
 end
