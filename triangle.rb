1aeda77825ef94d7213054680de133d713bb6dfa
Matheus Lima <matheus.limastack@outlook.com>
2021-08-26 12:57:25 -0300

    Completed Triangle 2

--- triangle.rb
+++ triangle.rb
@@ -22,7 +22,7 @@ refactoring_approach = <<-EOF
   {
     1 => :equilateral,
     2 => :isosceles,
-    3 => scalene
+    3 => :scalene
   }
  end
 
@@ -32,11 +32,14 @@ refactoring_approach = <<-EOF
 EOF
 
 def triangle(a, b, c)
+  @values = a, b, c
 
+  raise TriangleError if out_of_triangle_range?
 
-  return :equilateral if equilateral? a,b,c 
-  return :isosceles if isosceles? a,b,c
-  :scalene if scalene? a,b,c
+  return :equilateral if equilateral?
+  return :isosceles if isosceles?
+
+  :scalene if scalene?
 end
 
 puts refactoring_approach
@@ -45,14 +48,32 @@ puts refactoring_approach
 class TriangleError < StandardError
 end
 
-def scalene?(a,b,c)
-  [a,b,c].uniq.length == 3
+def out_of_triangle_range?
+  some_negative_value? || some_zero? || invalid_triangle_size?
 end
 
-def isosceles?(a,b,c)
-  [a,b,c].uniq.length == 2
+def scalene?
+  @values.uniq.length == 3
 end
 
-def equilateral?(a,b,c)
-  [a,b,c].uniq.length == 1
+def isosceles?
+  @values.uniq.length == 2
+end
+
+def equilateral?
+  @values.uniq.length == 1
+end
+
+def some_negative_value?
+  @values.any?(&:negative?)
+end
+
+def some_zero?
+  @values.any?(&:zero?)
+end
+
+def invalid_triangle_size?
+  !@values.filter do |value|
+    value >= @values.sum - value
+  end.empty?
 end
